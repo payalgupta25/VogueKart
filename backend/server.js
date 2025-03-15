c6df328afb2e4faf51d722f83e860b1bf590a26d
Payal <payalgupta425@gmail.com>
2025-03-15 16:34:08 +0530

    coupon routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { connectDB } from "./db/db.js";
 import authRoutes from "./routes/auth.routes.js"
 import productRoutes from "./routes/product.routes.js"
 import cartRoutes from "./routes/cart.routes.js"
+import couponRoutes from "./routes/coupon.routes.js"
 
 dotenv.config();
 const app=express()
@@ -18,6 +19,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products",productRoutes);
 app.use("/api/cart",cartRoutes);
+app.use("/api/coupons",couponRoutes)
 
 
 
