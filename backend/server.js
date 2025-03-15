75a4293ed692f44ba02f6fa71c4127ae3b408694
Payal <payalgupta425@gmail.com>
2025-03-15 15:30:45 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,28 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 import { connectDB } from "./db/db.js";
+
+import authRoutes from "./routes/auth.routes.js"
+import productRoutes from "./routes/product.routes.js"
+import cartRoutes from "./routes/cart.routes.js"
 
 dotenv.config();
 const app=express()
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products",productRoutes);
+app.use("/api/cart",cartRoutes);
+
+
+
 const PORT=process.env.PORT;
 app.listen(PORT,()=>{
-    console.log(`Server running on PORT ${PORT}`);
+    console.log(`Server running on PORT http://localhost:${PORT}`);
     connectDB();
 })
